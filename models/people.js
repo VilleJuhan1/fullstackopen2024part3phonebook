56add25483e79f3a4f6749f92a27d049efff3e49
Ville-Juhani Nivasalo <villejuhani.nivasalo@icloud.com>
2024-12-01 13:32:29 +0200

    validating data works and pops proper errors when needed

--- models/people.js
+++ models/people.js
@@ -14,8 +14,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 2
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
